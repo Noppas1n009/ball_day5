06917d10903c65e4839602320072b380a489a663
zeroplage <Apichatzeroball@gmail.com>
2022-02-03 10:43:26 +0700

    lv1

---
+++ Day5/page4.php
@@ -0,0 +1,14 @@
+<?php include_once 'head.php' ?>
+<?php include_once 'jumbotron.php';?>
+<?php include_once 'navigater.php';?>
+
+<div class="container" style="margin-top:30px">
+  <!-- <div class="row"> -->
+    <h3>อภิชาติ สีทน</h3>
+  <!-- </div> -->
+</div>
+
+<?php include_once 'footer.php' ?>
+
+</body>
+</html>
